e7d9caa4fa365ac9f369e3c09e1f4af311bf5e22
abhinanda2491 <s.rajatharadhya@gmail.com>
2023-04-07 17:26:32 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TAL.Net.Models;
+
+namespace TAL.Net.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("occupations")]
+    public ActionResult<IEnumerable<Occupation>> ListOccupations()
+    {
+        var occupations = new List<Occupation>
+        {
+            new Occupation { Name = "Cleaner", Rating = "Light Manual", Factor = 1.5 },
+            new Occupation { Name = "Doctor", Rating = "Professional", Factor = 1.0 },
+            new Occupation { Name = "Author", Rating = "White Collar", Factor = 1.25 },
+            new Occupation { Name = "Farmer", Rating = "Heavy Manual", Factor = 1.75 },
+            new Occupation { Name = "Mechanic", Rating = "Heavy Manual", Factor = 1.75 },
+            new Occupation { Name = "Florist", Rating = "Light Manual", Factor = 1.5 }
+        };
+
+        return Ok(occupations);
+    }
+
+}
+
---
+++ Models/Occupation.cs
@@ -0,0 +1,8 @@
+﻿namespace TAL.Net.Models;
+
+public class Occupation
+{
+    public string Name { get; set; }
+    public string Rating { get; set; }
+    public double Factor { get; set; }
+}
